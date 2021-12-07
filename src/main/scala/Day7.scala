6b7dd1512d44bc8e442d83b4b5a17e5750a3ab94
Mark Rood <mark.rood@nl.abnamro.com>
2021-12-07 07:03:26 +0100

    Day7 cleanup

--- src/main/scala/Day7.scala
+++ src/main/scala/Day7.scala
@@ -5,15 +5,12 @@ object Day7:
 
   def loadFile(): List[String] = Source.fromFile("src/main/scala/input/day7").getLines().toList.filter(str => str.nonEmpty).map(_.trim())
 
-  def bestPosition(fuelCostFunc: (Int, Int) => Int): Long =
-    val data = loadFile()
+  def bestPosition(fuelCostFunc: Int => Int): Long =
+    val crabs = loadFile().head.split(",").map(_.toInt)
 
-    val crabs = data.head.split(",").map(_.toInt)
-    val max = crabs.max
+    (0 to crabs.max).map(pos => crabs.map(c => fuelCostFunc(Math.abs(pos - c))).sum).min
 
-    (0 to max).map(pos => crabs.map(c => fuelCostFunc(pos, c)).sum).min
+  def run(): Unit = println(bestPosition(d => d))
 
-  def run(): Unit = println(bestPosition((x, y) => Math.abs(x - y)))
-
-  def runb(): Unit = println(bestPosition((x, y) => (Math.abs(x - y)* Math.abs(x - y) + Math.abs(x - y))/2 ))
+  def runb(): Unit = println(bestPosition(d => (d*d+d)/2))
 
